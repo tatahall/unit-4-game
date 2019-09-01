771eb035111a29e95d292262b1ec8ada6b87707c
Tahnee Hall <tnhall2012@hotmai.com>
2019-09-01 00:06:38 -0400

    updated documents with comments and psuedocode

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # unit-4-game
+
+Goal: To create a number guessing game with random values assigned to 4 different crystals with 'click' events and to generate a random computer number.  The player should be able to click on the crystals to try to match thier total score to the computers numbers.
+
---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+//goal of the game is to click on the crystals to match the
+//computers randomly generated number.
+
+//create variable for computer's random guess
+
+//create variables wins and losses
+
+//create variable for total score: total score should be an accumulation of points from the crystals clicked
+
+//create variables for crystals' random value. crystal value should be hidden, but don't think special
+//coding needed to hide them so much as not display them.
+
+//create variable/function for game reset:
+    //total score reset to 0;
+    //new computer guess generated;
+    //new crystal values/guesses generated;
+
+//if user's score === computer number:
+    //wins++
+    //game resets
+    //alert "you win"
+
+//if user's score !== computer number:
+    //losses++
+    //game resets
+    //alert "you lose"
