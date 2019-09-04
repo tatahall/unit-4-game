9ea5754b3038a9a901b3d88cd90ea2140e60b887
Tahnee Hall <tnhall2012@hotmai.com>
2019-09-04 12:23:34 -0400

    updated the js to add if statements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,8 +4,10 @@
 //create variable for computer's random guess between 19 and 120.
 //Math.floor(Math.random() * ((y-x)+1) + x); where x is start number and y is end number.
 var computerGuess = $("#computer-guess");//query html for computer guess id
+var minNumber = 19;
+var maxNumber = 120
 var assignComputerGuess = function () {
-    computerGuess = Math.floor(Math.random() * ((120 - 19) + 1) + 1);
+    computerGuess = Math.floor(Math.random() * (maxNumber + 1) + minNumber);
     $("#computer-guess").text("Computer's Guess:  " + computerGuess);
 }
 assignComputerGuess();
@@ -26,86 +28,94 @@ console.log(losses);
 //new computer guess generated;
 //new crystal values/guesses generated;
 var gameReset = function () {
-    totalScore = 0;
+    newTotalScoreCounter = 0;
     assignComputerGuess();
-    assignCanaryCrystal();
-    assignEmeraldCrystal();
-    assignRubyCrystal();
-    assignSapphireCrystal();
 }
 
-var totalScore = 0;
-function newTotalScore(value){
-    $("#total-score").text("Total Score: " + totalScore);
-    console.log(totalScore);
-}
-
-newTotalScore();
-console.log(newTotalScore);
+//create variable for #total-score
+var totalScoreCounter = 0;
+$("#total-score").text("Total Score: " + totalScoreCounter);
 
 //create variables for crystals' random value. 
 //Crystal Value should not be displayed. 
 //Value should be 1-12;
 //Need click event to add crystal value to total score: 4 click events?
 
-$("#green-button").on("click", function(){
+$("#green-button").on("click", function () {
     var emeraldCrystal = $("#green-button");
     var assignEmeraldCrystal = function () {
         emeraldCrystal = Math.floor(Math.random() * 12) + 1;
     }
     assignEmeraldCrystal();
     console.log(emeraldCrystal);
+    var newTotalScoreCounter = totalScoreCounter += emeraldCrystal;
+    $("#total-score").text("Total Score: " + newTotalScoreCounter);
+    console.log(newTotalScoreCounter);
+    if (totalScoreCounter === computerGuess) {
+        wins++;
+        $("#wins").text("Wins:  " + wins);
+        gameReset();
+        alert("You Win!");
+    }
+    
+    else if (totalScoreCounter > computerGuess) {
+        losses++;
+        $("#losses").text("Losses: " + losses);
+        gameReset();
+        alert("You Lose...Try Again!");
+    }    
 });
 
-/*
-$("#ruby-button").on("click", function(){
+$("#ruby-button").on("click", function () {
     var rubyCrystal = $("#ruby-button");
     var assignRubyCrystal = function () {
         rubyCrystal = Math.floor(Math.random() * 12) + 1;
     }
     assignRubyCrystal();
     console.log(rubyCrystal);
+    var newTotalScoreCounter = totalScoreCounter += rubyCrystal;
+    $("#total-score").text("Total Score: " + newTotalScoreCounter);
+    console.log(newTotalScoreCounter);
 });
 
-$("#yellow-button").on("click", function(){
+$("#yellow-button").on("click", function () {
     var canaryCrystal = $("#yellow-button");
-var assignCanaryCrystal = function () {
-    canaryCrystal = Math.floor(Math.random() * 12) + 1;
-}
-assignCanaryCrystal();
-console.log(canaryCrystal);
+    var assignCanaryCrystal = function () {
+        canaryCrystal = Math.floor(Math.random() * 12) + 1;
+    }
+    assignCanaryCrystal();
+    console.log(canaryCrystal);
+    var newTotalScoreCounter = totalScoreCounter += canaryCrystal;
+    $("#total-score").text("Total Score: " + newTotalScoreCounter);
+    console.log(newTotalScoreCounter);
 });
 
-$("#blue-button").on("click", function(){
+$("#blue-button").on("click", function () {
     var sapphireCrystal = $("#blue-button");
     var assignSapphireCrystal = function () {
         sapphireCrystal = Math.floor(Math.random() * 12) + 1;
     }
     assignSapphireCrystal();
     console.log(sapphireCrystal);
+    var newTotalScoreCounter = totalScoreCounter += sapphireCrystal;
+    $("#total-score").text("Total Score: " + newTotalScoreCounter);
+    console.log(newTotalScoreCounter);
 });
 
 
 //create variable for total score: 
 //total score should be an accumulation of points from the crystals clicks
 
-/*function score(){
-    var totalScore = 0;
-    var newTotalScore =  //this is wrong
-    $("#total-score").text("Total Score:  " + newTotalScore); // this is correct because I need to display
-    console.log(totalScore);
-   
-    if (newTotalScore === computersGuess){
-        wins++;
-        gameReset();
-        alert("You Win!");
-    }
-    
-    if (newTotalScore > computerGuess) {
-    
+/*if (totalScoreCounter === computerGuess) {
+    wins++;
+    $("#wins").text("Wins:  " + wins);
+    gameReset();
+    alert("You Win!");
+}
+
+else if (totalScoreCounter > computerGuess) {
     losses++;
+    $("#losses").text("Losses: " + losses);
     gameReset();
     alert("You Lose...Try Again!");
-}
-);}*/
-
+}*/
